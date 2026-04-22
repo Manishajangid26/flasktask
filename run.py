62e1211f310c153b82f6432da0ccef07ac77aee1
manisha jangid <manisahjangid12665@gmail.com>
2026-04-22 15:21:15 +0530

    update website

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from src import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=443)
+    context = ('/etc/letsencrypt/live/cakefactorykota.com/fullchain.pem',
+    '/etc/letsencrypt/live/cakefactorykota.com/privkey.pem')
+
+    # Run on port 443 with the SSL context
+    app.run(host='0.0.0.0', port=443, ssl_context=context)
